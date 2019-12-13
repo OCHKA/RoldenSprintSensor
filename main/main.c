d839bf887103f4d2eefff6a6da312ce7d7e3fff1
weith <turbavladimir@gmail.com>
2019-12-13 10:06:52 +0300

    Add external sensor trigger isr

--- main/main.c
+++ main/main.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <sys/unistd.h>
 
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-void app_main(void)
-{
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+const gpio_num_t GPIO_SENSOR_1 = GPIO_NUM_15;
+
+static uint32_t gpio_num;
+
+static void IRAM_ATTR sensor_isr(void *arg) {
+  uint32_t gpio_intr_status = READ_PERI_REG(GPIO_STATUS_REG);
+  uint32_t gpio_intr_status_h = READ_PERI_REG(GPIO_STATUS1_REG);
+  SET_PERI_REG_MASK(GPIO_STATUS_W1TC_REG, gpio_intr_status);
+  SET_PERI_REG_MASK(GPIO_STATUS1_W1TC_REG, gpio_intr_status_h);
+
+  if (gpio_intr_status & BIT(GPIO_SENSOR_1)) {
+    gpio_num = GPIO_SENSOR_1;
+  }
+}
+
+void app_main(void) {
+  gpio_config_t io_cfg;
+  io_cfg.intr_type = GPIO_INTR_POSEDGE;
+  io_cfg.mode = GPIO_MODE_INPUT;
+  io_cfg.pin_bit_mask = 1ULL << GPIO_SENSOR_1;
+  io_cfg.pull_down_en = 1;
+  gpio_config(&io_cfg);
+  gpio_isr_register(sensor_isr, NULL, ESP_INTR_FLAG_IRAM, NULL);
+
+  while (true) {
+    printf("GPIO: %u\n", gpio_num);
+    sleep(1);
+  }
 }
