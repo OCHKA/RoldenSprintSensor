9e2c857436e23322c5b5e35c2cdf21a8cdc844d8
weith <turbavladimir@gmail.com>
2019-12-16 09:35:30 +0300

    Add edge delay time measurement

--- main/main.c
+++ main/main.c
@@ -2,12 +2,13 @@
 #include <sys/unistd.h>
 
 #include <driver/gpio.h>
+#include <driver/timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
 const gpio_num_t GPIO_SENSOR_1 = GPIO_NUM_15;
 
-static uint32_t gpio_num;
+static uint64_t ticks_at_edge = 0;
 
 static void IRAM_ATTR sensor_isr(void *arg) {
   uint32_t gpio_intr_status = READ_PERI_REG(GPIO_STATUS_REG);
@@ -16,7 +17,7 @@ static void IRAM_ATTR sensor_isr(void *arg) {
   SET_PERI_REG_MASK(GPIO_STATUS1_W1TC_REG, gpio_intr_status_h);
 
   if (gpio_intr_status & BIT(GPIO_SENSOR_1)) {
-    gpio_num = GPIO_SENSOR_1;
+     timer_get_counter_value(0, TIMER_0, &ticks_at_edge);
   }
 }
 
@@ -29,8 +30,16 @@ void app_main(void) {
   gpio_config(&io_cfg);
   gpio_isr_register(sensor_isr, NULL, ESP_INTR_FLAG_IRAM, NULL);
 
+  timer_config_t tim_cfg;
+  tim_cfg.divider = 2;
+  tim_cfg.counter_dir = TIMER_COUNT_UP;
+  tim_cfg.counter_en = TIMER_START;
+  tim_cfg.alarm_en = TIMER_ALARM_DIS;
+  tim_cfg.auto_reload = TIMER_AUTORELOAD_EN;
+  timer_init(0, TIMER_0, &tim_cfg);
+
   while (true) {
-    printf("GPIO: %u\n", gpio_num);
+    printf("GPIO: %llu\n", ticks_at_edge);
     sleep(1);
   }
 }
