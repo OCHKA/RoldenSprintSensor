685a16d1d9dbfb6418bdf3ee2d856b4a7c5fd9c6
weith <turbavladimir@gmail.com>
2020-01-19 07:19:29 +0300

    Remove unnecessary debug output

--- main/main.cpp
+++ main/main.cpp
@@ -2,14 +2,10 @@
 #include <nvs_flash.h>
 #include <sys/unistd.h>
 
-#include <cstdio>
-
 #include "coap_server.hpp"
 #include "edge_period_sensor.hpp"
 #include "network.h"
 #include "selftest_gen.hpp"
-
-//#define DEBUG_PRINT_PERIODS
 
 extern "C" {
 void app_main(void) {
@@ -22,32 +18,8 @@ void app_main(void) {
   ESP_ERROR_CHECK(edge_period_sensor::init());
   ESP_ERROR_CHECK(selftest_gen::init());
 
-  auto col = 0;
   while (true) {
-#ifdef DEBUG_PRINT_PERIODS
-    auto period = edge_period_sensor::get_avg_period(0, 1000);
-#else
-    std::optional<bool> period;
     usleep(1000 * 1000);
-#endif
-
-    if (period) {
-      if (col) {
-        printf("\n");
-      }
-
-      printf("%u\n", period.value());
-      col = 0;
-    } else {
-      if (col > 80) {
-        col = 0;
-        printf("\n");
-      }
-
-      printf(".");
-      fflush(stdout);
-      col++;
-    }
   }
 }
 }
