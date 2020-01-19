f2dd4a53bac29925eadcee401020531fa659bab5
weith <turbavladimir@gmail.com>
2020-01-19 19:21:10 +0300

    Remove wifi event handler with useless output

--- main/network.c
+++ main/network.c
@@ -4,24 +4,6 @@
 #include <esp_wifi.h>
 #include <string.h>
 
-const char* TAG = "net";
-
-static void event_handler(void* arg,
-                          esp_event_base_t event_base,
-                          int32_t event_id,
-                          void* event_data) {
-  if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-    wifi_event_ap_staconnected_t* event =
-        (wifi_event_ap_staconnected_t*)event_data;
-    ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac),
-             event->aid);
-  } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-    wifi_event_ap_stadisconnected_t* event =
-        (wifi_event_ap_stadisconnected_t*)event_data;
-    ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d", MAC2STR(event->mac),
-             event->aid);
-  }
-}
 
 static void wifi_init_softap() {
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
@@ -35,7 +17,7 @@ static void wifi_init_softap() {
   ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
 
-  ESP_LOGI(TAG, "SoftAP SSID: %s password: %s",
+  ESP_LOGI("wifi", "SSID: %s password: %s",
            CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
 }
 
@@ -45,9 +27,6 @@ esp_err_t network_init() {
 
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                             &event_handler, NULL));
 
   wifi_init_softap();
 
